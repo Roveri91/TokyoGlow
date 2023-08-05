8218c628a916541cf753e1a8838522741d682180
Giulia Menin <giulia.menin@gmail.com>
2023-08-05 16:20:41 +0900

    user can create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    # @post = Post.new(post_params)
+    # @post.user = current_user
+    # @post.save!
+    @post = current_user.posts.create(post_params)
+    raise
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category)
+  end
 end
