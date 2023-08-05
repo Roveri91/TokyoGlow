03eb9835cddb72a881872658fc2c314ebb16ad1c
Jonny Smith <jonathanmartin.smith25@gmail.com>
2023-08-05 15:25:53 +0900

    finished basic hospital seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_05_034110) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_05_053433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_05_034110) do
     t.string "webpage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
   end
 
   create_table "posts", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Heartlessly destroying all the hospitals"
+Hospital.delete_all
+
+puts "Destroying all the USERS"
+User.delete_all
+
+users = []
+puts "Creating new Fabulous Users"
+
+user = User.create!(username: "Giulia", email: "GiuliaMenin@gmail.com", password: "123456")
+users << user
+puts "created user #{user.username}"
+
+user = User.create!(username: "Tenny", email: "TennyMa@gmail.com", password: "123456")
+users << user
+puts "created user #{user.username}"
+
+user = User.create!(username: "Jonny", email: "JonnySmith@gmail.com", password: "123456")
+users << user
+puts "created user #{user.username}"
+
+user = User.create!(username: "Simone", email: "SimoneRoveri@gmail.com", password: "123456")
+users << user
+puts "created user #{user.username}"
+
+puts "Building new hospitals. This could take a while..."
+
+hospital = Hospital.create!(name: "Tokyo Hospital", price_range: "10 - 1,000,000", services: "English speaking",
+   description: "This is the hospital for you", average_rating: 3.2, phone_number: "070-0707-0707" )
+puts "created #{hospital.name}"
+
+
+hospital = Hospital.create!(name: "Sanno Hospital", price_range: "20 - 30,000", services: "After child-care",
+    description: "The best hospital in Tokyo", average_rating: 4.5, phone_number: "123-4567-8901" )
+puts "created #{hospital.name}"
+
+hospital = Hospital.create!(name: "Imperial Hospital", price_range: "100 - 10,000,000", services: "Servants on hand 24/7",
+      description: "Hospital for Emperors", average_rating: 2.7, phone_number: "432-6534-6534-" )
+puts "created #{hospital.name}"
+
+puts "Done! Thank you for your patience."
