a81ca84ce448a692da473b9eef931d6648e2ea83
Giulia Menin <giulia.menin@gmail.com>
2023-08-05 15:26:48 +0900

    created 4 seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroying all the users"
+User.delete_all
+
+users = []
+puts "Creating new users"
+
+user = User.create!(email: "giuliamenin@gmail.com", password: "123456", username: "Giulia", due_date: "2023-12-01")
+users << user
+puts "created #{user.username} user"
+
+user = User.create!(email: "tennyma@gmail.com", password: "123456", username: "Tenny", due_date: "2023-12-02")
+users << user
+puts "created #{user.username} user"
+
+user = User.create!(email: "jonnySùmmith@gmail.com", password: "123456", username: "Jonny", due_date: "2023-12-03")
+users << user
+puts "created #{user.username} user"
+
+user = User.create!(email: "simoneroveri@gmail.com", password: "123456", username: "Simone", due_date: "2023-12-04")
+users << user
+puts "created #{user.username} user"
+
+puts "Done"
