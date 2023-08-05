d649f471d3a01d854ee8581487f50013b9c7097c
Giulia Menin <giulia.menin@gmail.com>
2023-08-05 15:50:50 +0900

    new for posts is done

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new]
 end
