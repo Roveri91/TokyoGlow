7e635db8db1731709f38a2827122e00785075255
Tenny Ma <128113562+tentenma@users.noreply.github.com>
2023-08-05 18:39:05 +0900

    edited pundit for hospitals and pots

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   include Pundit::Authorization
 
   # Pundit: allow-list approach
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
-  
+
   private
 
   def skip_pundit?
