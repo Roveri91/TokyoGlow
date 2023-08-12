95a164d348a743faa4aeec5fdfe7931d886272b8
Jonny Smith <jonathanmartin.smith25@gmail.com>
2023-08-12 14:01:37 +0900

    finished displaying appointments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,17 @@ class UsersController < ApplicationController
 
   def calendar
     authorize @profile
+
+    if params[:date]
+      date = params[:date]
+    else
+      date = Date.today.strftime("%Y-%m-%d")
+    end
+    @current_day_appointments = Appointment.where(user: current_user, date: date)
     @appointments = Appointment.where(user: current_user)
+
+    # year = Date.parse(date).year       WILL NEED THIS FOR EVENTS
+    # month = Date.parse(date).month
   end
 
   private
@@ -19,9 +29,9 @@ end
 
 
 # NEED LATER
-# link for the google calendar 
+# link for the google calendar
 # <a href="http://www.google.com/calendar/event?action=TEMPLATE&text=[event-title]&dates=[start-custom format='Ymd\\THi00\\Z']/[end-custom format='Ymd\\THi00\\Z']&details=[description]&location=[location]&trp=false&sprop=&sprop=name:" target="_blank" rel="nofollow">Add to my calendar</a>
 
 
-# 
+#
 # https://support.google.com/calendar/answer/41207?hl=en&ref_topic=10510447&sjid=16995394533718824250-AP
