79eda92c4f08d8b6089c455acacd3c25c3f274ac
Giulia Menin <giulia.menin@gmail.com>
2023-08-12 13:47:30 +0900

    improved hospital seeds - missing images

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,19 +51,54 @@ puts "Hospitals destroyed. People are crying"
 puts "Building new hospitals. This could take a while..."
 
 hospital_image = "default_hospital_image.jpg"
-locations = %w[Tokyo Nakano Nogizaka Harajuku Shinjuku]
+locations = %w[Adachi Arakawa Bunkyo Chiyoda Chuo Edogawa Itabashi Katsushika Kita Koto Minato Meguro Nakano Nerima Ota Setagaya Shibuya Shinagawa Shinjuku Suginami Sumida Taito Toshima]
+price_range = %w[$ $$ $$$ $$$$ $$$$$]
+services = ["English Speaking", "Interpretation Services", "Private rooms", "Fertility treatments", "Normal deliveries", "Assistance with breastfeeding", "Epidural anesthesia", "In-vitro fertilization", "Embryo transfers", "After child-care", "Servants on hand 24/7", "Kangaroo care", "Post-partum depression assistance"]
 
-hospital = Hospital.create!(name: "Tokyo Hospital", address:locations.sample, price_range: "10 - 1,000,000", services: "English speaking",
-   description: "This is the hospital for you", average_rating: 3.2, phone_number: "070-0707-0707", default_image: hospital_image )
+hospital = Hospital.create!(name: "Tokyo Hospital", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+   description: "We provide advanced medical treatment in a variety of fields with warm hospitality.", average_rating: 3.2, phone_number: "03-0707-0707", default_image: hospital_image)
 puts "created #{hospital.name}"
 
-
-hospital = Hospital.create!(name: "Sanno Hospital", address:locations.sample, price_range: "20 - 30,000", services: "After child-care",
-    description: "The best hospital in Tokyo with a really long description so I can see how long it looks on the index page. But actually it needs to be a little bit longer so it goes beyond the card", average_rating: 4.5, phone_number: "123-4567-8901", default_image: hospital_image )
+hospital = Hospital.create!(name: "Sanno Hospital", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+    description: " A medical check-up center, a clinic for internal medicine, pediatrics, and orthopedics, and a rehabilitation center. We are dedicated to providing assistance to the Residents of Tokyo to live safely in their familiar environment. ", average_rating: 4.5, phone_number: "03-0612-0770", default_image: hospital_image )
 puts "created #{hospital.name}"
 
-hospital = Hospital.create!(name: "Imperial Hospital", address:locations.sample, price_range: "100 - 10,000,000", services: "Servants on hand 24/7",
-      description: "Hospital for Emperors", average_rating: 2.7, phone_number: "432-6534-6534", default_image: hospital_image )
+hospital = Hospital.create!(name: "Aiiku Hospital", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+    description: "In order to ensure the happiness and healthy growth of all mothers and babies, Aiiku Hospital provides high-quality medical care and supports a safe and comfortable pregnancy, labor/delivery, and child-rearing. ", average_rating: 2.7, phone_number: "03-4642-8846", default_image: hospital_image)
+puts "created #{hospital.name}"
+
+hospital = Hospital.create!(name: "Tokyo Takanawa Hospital", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+  description: "The Department of Obstetrics and Gynecology provides medical care for all phases of a woman’s life from puberty to pregnancy, child birth, and menopause, with a main focus on reproduction. ", average_rating: 3.7, phone_number: "03-3324-1123", default_image: hospital_image)
+puts "created #{hospital.name}"
+
+hospital = Hospital.create!(name: "Tokyo Midtown Clinic", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+  description: "In addition to a wide range of medical practices, the Tokyo Midtown Clinic boasts a special outpatient center, next-generation Health Screening Center and more. We provide total health support for our patients. ", average_rating: 2.9, phone_number: "03-5477-3426", default_image: hospital_image)
+puts "created #{hospital.name}"
+
+hospital = Hospital.create!(name: "Japan Red Cross Medical Center", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+  description: " The baby and parents play a very important role of childbirth. As a baby- and mother-friendly hospital,
+we attach great importance to perinatal safety, and we draw out maternal instinct at childbirth and
+during childcare in order to help the baby live and grow healthily", average_rating: 3.7, phone_number: "03-6667-2243", default_image: hospital_image)
+puts "created #{hospital.name}"
+
+hospital = Hospital.create!(name: "Jikei Hospital, Women's and Children's Center", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+  description: " Midwives and nurses promote breastfeeding and will help you after delivery. Breastfeeding room with pumps also available.", average_rating: 4.7, phone_number: "03-1123-6688", default_image: hospital_image)
+puts "created #{hospital.name}"
+
+hospital = Hospital.create!(name: "Seibo Catholic Hospital", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+  description: "Allow for freedom to labor in different positions if no epidural. Birth partner can be in the room with you (but only husband, not other family members). Epidural available during certain hours and on week days.", average_rating: 1.7, phone_number: "03-6890-2200", default_image: hospital_image)
+puts "created #{hospital.name}"
+
+hospital = Hospital.create!(name: "Seijo Kinoshita Hospital", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+  description: " Dr. Kinoshita is known for being quite flexible with laboring positions. Modern, updated facility. Smaller hospital with a focus on maternal and pediatric care, among other things.", average_rating: 4.0, phone_number: "03-8899-4598", default_image: hospital_image)
+puts "created #{hospital.name}"
+
+hospital = Hospital.create!(name: "Nagai Mother’s Hospital", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+  description: "We give the option to voluntarily induce labor. On demand epidural, no reservation needed. Also same cost with or without the epidural. One person is allowed in the room with you during labor.", average_rating: 3.9, phone_number: "03-4422-5557", default_image: hospital_image)
+puts "created #{hospital.name}"
+
+hospital = Hospital.create!(name: "Kanto Rozai Hospital", address:locations.sample, price_range: price_range.sample, services: services.sample(4),
+  description: "We encourage kangaroo care after birth and breastfeeding. English support may not be available – please enquire about availability in advance. We accept all major credit cards.", average_rating: 2.9, phone_number: "03-3456-9723", default_image: hospital_image)
 puts "created #{hospital.name}"
 
 puts "Done! Thank you for your patience."
