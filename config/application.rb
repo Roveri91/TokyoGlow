7e252797c7ea1298054705fef86948f3c333da53
Simone Roveri <spikeroveri91@gmail.com>
2023-08-30 15:43:38 +0900

    set timezone as Japan

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module TokyoGlow2
   class Application < Rails::Application
+    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
     config.generators do |generate|
       generate.assets false
       generate.helper false
