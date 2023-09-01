0d06bc7e8193941f3f45e4ddd528541a81b882d3
Simone Roveri <spikeroveri91@gmail.com>
2023-09-01 13:29:47 +0900

    dot in calendar are working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,8 @@ class UsersController < ApplicationController
     @current_day_events = Event.where(user: current_user, date: date)
     @events = Event.where(user: current_user)
     @attending = Event.joins(:attendants).where('attendants.user_id' => current_user.id, 'attendants.status' => 1)
-
+    # @attending = Attendant.where(current_user.attendants.status == "attending", event != current_user) 
+    @events_attending_not_created = current_user.attendants.select { |a| a.event.user != current_user }
     # year = Date.parse(date).year       WILL NEED THIS FOR EVENTS
     # month = Date.parse(date).month
     unless current_user.appointments.empty?
