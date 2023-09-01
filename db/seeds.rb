3fb5558484f1f3c534b6e9a1fee6219c847044ea
Simone Roveri <spikeroveri91@gmail.com>
2023-09-01 11:45:43 +0900

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -304,7 +304,7 @@ clementine = User.find_by(username: 'Clementine')
 lola = User.find_by(username: 'Lola')
 
 def generate_random_time
-  hours = (9..21).to_a
+  hours = (9..18).to_a
   random_hour = hours.sample
   random_minute = (0..3).to_a.sample * 15
   random_time = Time.new(2000, 1, 1, random_hour, random_minute)
