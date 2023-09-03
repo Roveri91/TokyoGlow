aa47901aab9f577636cb7d2169bdeb1a6890cd74
Simone Roveri <spikeroveri91@gmail.com>
2023-09-03 14:06:33 +0900

    need to focus the page at the buttom since the begin

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, :conversation_id, :user_id, presence: true
+
+  def sender?(a_user)
+    user.id == a_user.id
+  end
 end
